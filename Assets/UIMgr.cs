7e94f2d903acc51258b52b7755e8c697ac478a7e
happyyi <immb@qq.com>
2021-01-04 13:36:27 +0800

    Impl UI

---
+++ Assets/UIMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMgr : MonoBehaviour
+{
+    public GameObject MainUI;
+    public GameObject PlotUI;
+
+    public void OnClickPlotButton()
+    {
+        MainUI?.SetActive(false);
+        PlotUI?.SetActive(true);
+    }
+
+    public void OnClickNumberButton()
+    {
+        MainUI?.SetActive(true);
+        PlotUI?.SetActive(false);
+    }
+}
